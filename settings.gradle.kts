0cea2559f46f29c063034e35ff9dee6bdac6277f
Ansalpandey <pandeyansal77@gmail.com>
2025-02-09 22:03:09 +0530

    add Prometheus scrape configuration for Spring Boot Security DB metrics

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,8 @@
-rootProject.name = "SpringSecurityDB"
+scrape_configs:
+  - job_name: 'SpringBootSecurityDBMetrics'
+    metrics_path: '/actuator/prometheus'
+    scrape_interval: 3s
+    static_configs:
+      - targets: ['host.docker.internal:8080']
+        labels:
+          application: 'springbootsecuritydb'
